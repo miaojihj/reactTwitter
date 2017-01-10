bd581c19851dcd984d8edf8cb37fcc648abba603
locky <lockyhj@126.com>
2017-01-10 21:05:31 +0800

    change code

--- source/components/Stream.react.js
+++ source/components/Stream.react.js
@@ -10,5 +10,35 @@ var Stream = react.createClass({
     }
   },
 
-  compo
-})
+  componentDidMount:function(){
+  	SnapkiteStreamClient.initializeStream(this.handleNewTweet);
+  },
+
+  componentWillUnmount:function(){
+  	SnapkiteStreamClient.destoryStream();
+  },
+
+  handleNewTweet:function(tweet){
+  	this.setState({
+  		tweet:tweet
+  	});
+  },
+
+  render:function(){
+  	var tweet=this.state.tweet;
+
+  	if(tweet){
+  		return(
+  			<StreamTweet
+  				tweet={tweet}
+  				onAddTweetToCollection={this.props.onAddTweetToCollection} />
+  		);
+  	}
+  	return(
+  		<Header text="Waiting for public photos from Twitter..." />
+  	);
+  }
+
+});
+
+module.exports=Stream;
