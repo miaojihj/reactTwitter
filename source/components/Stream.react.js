1710c501f773f84a5997b6cdedeceb8a3dbe1431
locky <miaojihj@126.com>
2017-01-10 09:08:39 +0800

    20170110

---
+++ source/components/Stream.react.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var SnapkiteStreamClient = require('snapkite-stream-client');
+var StreamTweet = require('./StreamTweet.react');
+var Header = require('./Header.react');
+
+var Stream = react.createClass({
+  getInitialState:function(){
+    return{
+      tweet:null
+    }
+  },
+
+  compo
+})
